dff57c4ea1a34da188b65c386bc29bd432910046
Phuc-EroDev <phucvo140902@gmail.com>
2025-05-12 17:59:12 +0700

    make card

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Card } from 'antd';
-import Meta from 'antd/es/card/Meta';
+import { StarFilled } from '@ant-design/icons';
+import { StyleNameProduct, WrapperCardStyle, WrapperDiscountText, WrapperPriceText, WrapperReportText } from './style';
+import labelCard from '../../assets/images/labelCard.png';
 
 const CardComponent = () => {
   return (
-    <Card
+    <WrapperCardStyle
       hoverable
-      style={{ width: 240 }}
+      headStyle={{ width: '200px', height: '200px' }}
+      style={{ width: 200 }}
+      bodyStyle={{ padding: '10px' }}
       cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
     >
-      <Meta title="Europe Street beat" description="www.instagram.com" />
-    </Card>
+      <img src={labelCard} style={{ width: '25px', height: '18px', position: 'absolute', top: '5px', left: '-4px' }} />
+      <StyleNameProduct>IPhone</StyleNameProduct>
+      <WrapperReportText>
+        <span style={{ marginRight: '4px' }}>
+          <span>4.99</span> <StarFilled style={{ fontSize: '12px', color: 'yellow' }} />
+        </span>
+        <span> | Da ban 1000+</span>
+      </WrapperReportText>
+      <WrapperPriceText>
+        1.000.000 VND <WrapperDiscountText>-50%</WrapperDiscountText>
+      </WrapperPriceText>
+    </WrapperCardStyle>
   );
 };
 
---
+++ src/components/CardComponent/style.js
@@ -0,0 +1,38 @@
+import { Card } from "antd";
+import styled from "styled-components";
+
+export const WrapperCardStyle = styled(Card)`
+    width: 200px;
+    & img {
+      width: 200px;
+      height: 200px;
+    },
+    position: relative;
+`
+
+export const StyleNameProduct = styled.div`
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: #171414;
+`
+
+export const WrapperReportText = styled.div`
+    font-size: 10px;
+    color: #171414;
+    display: flex;
+    align-items: center;
+    margin-top: 6px;
+`
+
+export const WrapperPriceText = styled.div`
+    color: #e53030;
+    font-size: 16px;
+    font-weight: 500;
+`
+
+export const WrapperDiscountText = styled.span`
+    color: #f54141;
+    font-size: 12px;
+    font-weight: 500;
+`
